c600918521bfbfc296f1255f4f7a69fb64e939c7
Matheus Sleutjes <matheus.sleutjes@loglabdigital.com.br>
2024-09-25 22:02:20 -0400

    criação de view e metodos

--- VirtualShop/ecom/produtos/models.py
+++ VirtualShop/ecom/produtos/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Produto(models.Model):
+    produto_nome = models.CharField(max_length=100)
+    produto_descricao = models.TextField()
+    produto_imagem = models.ImageField(upload_to="produtos")
--- VirtualShop/ecom/produtos/views.py
+++ VirtualShop/ecom/produtos/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from models import *
 
-# Create your views here.
+def produtos(request):
+    if request.method == 'POST':
+        data = request.POST
+        produto_nome = data.get('produto_nome')
+        produto_descricao = data.get('produto_descricao')
+        produto_imagem = data.FILES.get('produto_imagem')
+
+        Produto.objects.create(
+            produto_nome = produto_nome,
+            produto_descricao = produto_descricao,
+            produto_imagem = produto_imagem
+        )
+        return redirect('/')
+
+def deletar_produtos(request):
+    query = Produto.objects.get(id=id)
+    query.delete()
+    return redirect('/')
